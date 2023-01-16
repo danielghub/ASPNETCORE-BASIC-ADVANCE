ff3166a5fda126aea5def0848f1346d64c26feab
Daniel Barles <danielbarles24@gmail.com>
2023-01-17 04:32:54 +0800

    Adding a new file for Route Constraints

---
+++ assignment1/RouteConstaints/RouteConstraint.cs
@@ -0,0 +1,8 @@
+﻿namespace assignment1.RouteConstaints
+{
+    //Implement rounte logic here
+    // should've add this class in the map program class 
+    public class RouteConstraints
+    {
+    }
+}
